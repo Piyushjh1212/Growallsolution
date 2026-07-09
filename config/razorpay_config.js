c23d55d9ed7c39cce2a34c2477ac7e4c702c242f
Piyush Jhariya <piyushjhariya65@gmail.com>
2026-07-09 23:19:41 +0530

    update razorpay

--- config/razorpay_config.js
+++ config/razorpay_config.js
@@ -4,8 +4,7 @@ const RAZORPAY_CONFIG = {
     // Apni Razorpay Test ya Live Key yahan daalein
     key_id: "rzp_live_T9u5Bqd0qBcSbl", 
     currency: "INR",
-    company_name: "Grow All Coaching Group",
-    description: "Premium Masterclass Bundle",
-    theme_color: "#3b82f6", // Aapki website ke blue theme se match karta hua
-    company_logo: "https://your-website.com/logo.png" // Optional
+    company_name: "Grow All Coaching",
+    theme_color: "#3b82f6",
+    company_logo: "https://res.cloudinary.com/dieboinjz/image/upload/v1772387672/GacImages/cjgywtxrv1g6etyve2rl.jpg"
 };
